5085bab10293a30e848b07451c1f970f1625f680
Hais Rodolphe <44665224+HaisRodolphe@users.noreply.github.com>
2020-02-05 08:24:05 +0100

    Udapte index.php

--- php/index.php
+++ php/index.php
@@ -7,17 +7,33 @@
 </head>
 
 <body>
-<?php
+  <h1>Contact</h1>
+  <form method="post">
+    <label>Nom</label>
+    <input type="text" name="nom" required>
+    <label>Email</label>
+    <input type="email" name="email" required>
+    <label>Message</label>
+    <textarea name="message" required></textarea>
+    <input type="submit">
+  </form>
+  <?php
+  if (isset($_POST['message'])) {
+    $entete  = 'MIME-Version: 1.0' . "\r\n";
+    $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+    $entete .= 'From: ' . $_POST['email'] . "\r\n";
 
-if(isset($_POST['Send Message'])){
-    $Name = $_POST['Name'];
-    $Email = $_POST['Email'];
-    $Subject = $_POST['Subject'];
-    $Message = $_POST['Message'];
-    print("<center>Bonjour $Name $Email $Subject $Message</center>");
-}    
-?>
+    $message = '<h1>Message envoyé depuis la page Contact de monsite.fr</h1>
+        <p><b>Nom : </b>' . $_POST['nom'] . '<br>
+        <b>Email : </b>' . $_POST['email'] . '<br>
+        <b>Message : </b>' . $_POST['message'] . '</p>';
 
+    $retour = mail('rodolphe.hais@gmail.com', 'Envoi depuis page Contact', $message, $entete);
+    if ($retour) {
+      echo '<p>Votre message a bien été envoyé.</p>';
+    }
+  }
+  ?>
 </body>
 
 </html>
